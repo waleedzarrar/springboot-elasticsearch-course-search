9a85758972693dd0d9ff0c03331d3e3358253c33
waleed <waleedzarrar5@gmail.com>
2025-07-14 00:24:15 +0530

    Assignment A completed: search, pagination, sorting implemented and tested

--- src/main/java/com/undoschool/course_search/document/CourseDocument.java
+++ src/main/java/com/undoschool/course_search/document/CourseDocument.java
@@ -3,49 +3,37 @@ package com.undoschool.course_search.document;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.DateFormat;
-import org.springframework.data.elasticsearch.annotations.FieldType;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-@Document(indexName = "courses")
 @Data
 @Builder
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Document(indexName = "courses")
 public class CourseDocument {
 
     @Id
     private String id;
 
-    @Field(type = FieldType.Text)
     private String title;
 
-    @Field(type = FieldType.Text)
     private String description;
 
-    @Field(type = FieldType.Keyword)
     private String category;
 
-    @Field(type = FieldType.Keyword)
     private String type;
 
-    @Field(type = FieldType.Keyword)
     private String gradeRange;
 
-    @Field(type = FieldType.Integer)
-    private int minAge;
+    private Integer minAge;
 
-    @Field(type = FieldType.Integer)
-    private int maxAge;
+    private Integer maxAge;
 
-    @Field(type = FieldType.Double)
-    private double price;
+    private Double price;
 
-    @Field(type = FieldType.Date, format = DateFormat.date_time)
-    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     private LocalDateTime nextSessionDate;
+
+    private List<String> suggest;  // used by completion suggester
 }
